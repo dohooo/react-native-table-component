c9ff3b6f1155387349516b6820ee7d72fc2712c8
鸡斯拉 <594244274@qq.com>
2018-03-29 17:11:26 +0800

    edit

--- example/src/react-native-table-component/index.js
+++
@@ -1,6 +0,0 @@
-import { Row, Rows } from './components/rows';
-import { Col, Cols } from './components/cols';
-import { Table, TableWrapper } from './components/table';
-import Cell from './components/cell';
-
-export { Table, TableWrapper, Row, Rows, Col, Cols, Cell };
